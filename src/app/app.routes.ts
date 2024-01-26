d6a9485f057cb5bb1be685d3198a6556415d281b
JessCP25 <jpayanoc25@gmail.com>
2024-01-25 23:58:22 -0500

    Modificante las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,47 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'reactive',
+    children: [
+      // {
+      //   path: '',
+      //   redirectTo: 'basic',
+      //   pathMatch: 'full'
+      // },
+      {
+        path: 'basic',
+        loadComponent: () => import('./reactive/pages/basic/basic.component').then(c => c.BasicComponent)
+      },
+      {
+        path: 'dynimac',
+        loadComponent: () => import('./reactive/pages/dynamic/dynamic.component').then(c => c.DynamicComponent)
+      },
+      {
+        path: 'switches',
+        loadComponent: () => import('./reactive/pages/switches/switches.component').then(c => c.SwitchesComponent)
+      },
+      {
+        path: '**',
+        redirectTo: 'basic'
+      }
+    ]
+  },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'sign-up',
+        loadComponent: () => import('./auth/pages/register/register.component').then(c => c.RegisterComponent)
+      },
+      {
+        path: '**',
+        redirectTo: 'sign-up'
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'reactive'
+  }
+];
