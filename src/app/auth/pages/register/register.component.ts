a850cfc20422814070e8085e0c22739219fd52a5
JessCP25 <jpayanoc25@gmail.com>
2024-02-02 16:21:01 -0500

    Validators functions

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { cantBeStrider } from '../../../shared/validators/validators';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styles: `
     :host {
@@ -14,4 +19,20 @@ import { Component } from '@angular/core';
     }
   `,
 })
-export class RegisterComponent { }
+export class RegisterComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required, cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  isValdField(field: string) {}
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+}
