2d4c2ee14fc58b8d022a3523fe18f3f9f014a479
JessCP25 <jpayanoc25@gmail.com>
2024-02-02 14:41:20 -0500

    Desestructurar valores de myForm

--- src/app/reactive/pages/switches/switches.component.ts
+++ src/app/reactive/pages/switches/switches.component.ts
@@ -1,12 +1,16 @@
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './switches.component.html',
   styles: `
     :host {
@@ -14,4 +18,30 @@ import { Component } from '@angular/core';
     }
   `,
 })
-export class SwitchesComponent { }
+export class SwitchesComponent {
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    const {termsAndConditions, ...newPerson} = this.myForm.value;
+
+    console.log(this.myForm.value);
+    console.log(newPerson);
+  }
+
+  isInvalidField(field: string) {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+}
